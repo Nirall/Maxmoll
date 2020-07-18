3cc575d37a00fac7670125b971522504ce1dee5a
Aleksey <nirall@mail.ru>
2020-07-18 22:16:06 +0500

    Homepage is completed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import "./style.scss";
 
 import "./images/logo.png";
 import "./images/bucket.jpg";
-import "./images/dropdown.jpg";
+import "./images/bg-homepage.png";
+import "./images/360.png";
+import "./images/paragram.png";
+import "./images/snake.png";
+import "./images/squares.png";
+
+
+import "./fonts/BebasNeueRegular.eot";
+import "./fonts/BebasNeueRegular.ttf";
+import "./fonts/BebasNeueRegular.woff";

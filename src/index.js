72472e14c9e2722f9158b0d86ae815c4d5787c99
Aleksey <nirall@mail.ru>
2020-07-20 22:17:16 +0500

    Layouts are almost done

--- src/index.js
+++ src/index.js
@@ -9,6 +9,17 @@ import "./images/paragram.png";
 import "./images/snake.png";
 import "./images/squares.png";
 import "./images/img1.png";
+import "./images/share.png";
+import "./images/vk.png";
+import "./images/fb.png";
+import "./images/tw.png";
+import "./images/ok.png";
+import "./images/inst.png";
+import "./images/avatar1.jpg";
+import "./images/50per.png";
+import "./images/hit.png";
+import "./images/new.png";
+import "./images/teaserImg1.png";
 
 import "./fonts/BebasNeueRegular.eot";
 import "./fonts/BebasNeueRegular.ttf";
